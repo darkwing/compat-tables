e9663a5d5752f40bf1a21f6f4d10d6399c771d97
David Walsh <davidwalsh83@gmail.com>
2015-05-01 14:25:27 -0500

    Add icons for support cells

--- compat-tables.js
+++ compat-tables.js
@@ -1,8 +1,15 @@
 /*
 
+    TO DO
+    ===============================
+
+    1.  
+
+
     From Whitlock, to get cell data:
 
-    meta.compat_table.supports[feature.id][browser.id] = [support1.id, support2.id]
+        -  meta.compat_table.supports[feature.id][browser.id] = [support1.id, support2.id]
+        -  The last item in the array is the most current, everything else is relevant "history"
 
 */
 
@@ -16,7 +23,11 @@ function loadTable(payload, locale, isDebug) {
     // Important Vars
     var numberOfBrowsers = getNumberOfBrowsers();
 
+    // This is ultimately returned from the function
     var output = '';
+
+    // Templates
+    var iconTemplate = '<abbr title="{title}" class="only-icon"><span>{text}</span><i class="ic-{icon}" aria-hidden="true"></i></abbr>';
 
     // TABLE
     // ===============================
@@ -43,7 +54,8 @@ function loadTable(payload, locale, isDebug) {
         tab.browsers.forEach(function(browserId) {
             var matchedBrowserObj = findObjectByIdInArray(browserId, payload.linked.browsers);
             var browserName = getLocaleOrDefaultFromObject(matchedBrowserObj.name);
-            var slugForCss = matchedBrowserObj.slug; // Kind of a big assumption
+            
+            var slugForCss = matchedBrowserObj.slug; // PROBLEM:  Kind of a big assumption
 
             output += '<th class="bc-browser-' + slugForCss + '-HELP">'; // PROBLEM:  No way to  identify "bc-browser-{browser}-{env}" for class
             output += '<abbr class="only-icon" title="' + browserName +'">';
@@ -64,12 +76,48 @@ function loadTable(payload, locale, isDebug) {
     // ===============================
     output += '<tbody>';
     payload.linked.features.forEach(function(feature) {
+
+        console.warn(feature);
+
         output += '<tr>';
-        output += '<th scope="row">' + getLocaleOrDefaultFromObject(feature.name) + (isDebug ? '<br><code class="debug-detail">(' + feature.id + ')</code>' : '') + '</th>';
+        output += '<th scope="row">';
+        output += getLocaleOrDefaultFromObject(feature.name);
+
+        // Account for icons in this cell
+        if(feature.experimental || feature.standardized || feature.obsolete) {
+            output += '<div class="bc-icons">';
+            if(feature.experimental) {
+                output += substitute(iconTemplate, {
+                    title: '{{ PROBLEM }}',
+                    text: '{{ PROBLEM }}',
+                    icon: 'experimental'
+                });
+            }
+            if(feature.standardized === false) {
+                output += substitute(iconTemplate, {
+                    title: '{{ PROBLEM }}',
+                    text: '{{ PROBLEM }}',
+                    icon: 'standardized'
+                });
+            }
+            if(feature.obsolete) {
+                output += substitute(iconTemplate, {
+                    title: '{{ PROBLEM }}',
+                    text: '{{ PROBLEM }}',
+                    icon: 'obsolete'
+                });
+            }
+            output += '</div>';
+        }
+
+        if(isDebug) {
+            output += '<br><code class="debug-detail">(' + feature.id + ')</code>';
+        }
+        output += '</th>';
 
         tabs.forEach(function(tab) {
             tab.browsers.forEach(function(browserId) {
-                var browserFeatureResult = payload.meta.compat_table.supports[feature.id][browserId];
+                var browserFeatureHistory = payload.meta.compat_table.supports[feature.id][browserId];
                 var currentBrowserObj;
 
                 var cell = {
@@ -77,13 +125,13 @@ function loadTable(payload, locale, isDebug) {
                     content: ''
                 };
 
-                log(feature.id, '|', browserId, '|', browserFeatureResult);
+                log(feature.id, '|', browserId, '|', browserFeatureHistory);
 
-                if(browserFeatureResult) {
+                if(browserFeatureHistory) {
 
                     // Assume the last item is the "current"
                     // This will likely need to change in the future
-                    currentBrowserObj = findObjectByIdInArray(browserFeatureResult.pop(), payload.linked.supports);
+                    currentBrowserObj = findObjectByIdInArray(browserFeatureHistory.pop(), payload.linked.supports);
 
                     // Determine support via classname
                     // PROBLEM:  Need to add  bc-browser-{browser}-{desktop|mobile} (ex: "bc-browser-firefox-desktop")
@@ -93,19 +141,82 @@ function loadTable(payload, locale, isDebug) {
                     // This is going to need a ton of logic 
                     cell.content += '(' + currentBrowserObj.support + ')'; // PROBLEM:  This requires localization for "Yes" and "No"
 
+                    // Account for any required icons
+                    if( currentBrowserObj.prefix_mandatory || 
+                        currentBrowserObj.alternate_name_mandatory || 
+                        currentBrowserObj['protected'] || 
+                        currentBrowserObj.notes ||
+                        (currentBrowserObj.requires_config && currentBrowserObj.default_config && (currentBrowserObj.requires_config != currentBrowserObj.default_config))
+                    ) {
+
+                        cell.content += '<div class="bc-icons">';
+
+                        // Browser Prefix
+                        if(currentBrowserObj.prefix_mandatory) {
+                            cell.content += substitute(iconTemplate, {
+                                title: '{{ PROBLEM }}',
+                                text: '{{ PROBLEM }}',
+                                icon: 'prefix'
+                            });
+                        }
+
+                        // Alternate Name Mandatory
+                        if(currentBrowserObj.alternate_name_mandatory) {
+                            cell.content += substitute(iconTemplate, {
+                                title: '{{ PROBLEM }}',
+                                text: '{{ PROBLEM }}',
+                                icon: 'altname'
+                            });
+                        }
+
+                        // Requires/Default config
+                        if(currentBrowserObj.requires_config && currentBrowserObj.default_config && (currentBrowserObj.requires_config != currentBrowserObj.default_config)) {
+                            cell.content += substitute(iconTemplate, {
+                                title: '{{ PROBLEM }}',
+                                text: '{{ PROBLEM }}',
+                                icon: 'disabled'
+                            });
+                        }
+
+                        // Protected
+                        if(currentBrowserObj['protected']) {
+                            cell.content += substitute(iconTemplate, {
+                                title: '{{ PROBLEM }}',
+                                text: '{{ PROBLEM }}',
+                                icon: 'protected'
+                            });
+                        }
+
+                        // Notes
+                        if(currentBrowserObj.notes) {
+                            cell.content += substitute(iconTemplate, {
+                                title: '{{ PROBLEM }}',
+                                text: '{{ PROBLEM }}',
+                                icon: 'footnote'
+                            });
+                        }
+
+                        cell.content += '</div>';
+
+                    }
+
+
+                    // History stuff goes here
+
+
                     // Determine support
                     log('current', currentBrowserObj);
 
                     // Just debug
-                    //browserFeatureResult.forEach(function(id) { log(findObjectByIdInArray(id, payload.linked.supports)); });
+                    //browserFeatureHistory.forEach(function(id) { log(findObjectByIdInArray(id, payload.linked.supports)); });
                 }
                 else {
                     cell.className = 'bc-supports-unknown';
-                    cell.content += '(none)';
+                    cell.content += '?';
                 }
 
                 if(isDebug) {
-                    cell.content += '<br><br><br><br><br><code class="debug-detail-alt" title="feature id">' + feature.id + '</code> <code class="debug-detail-alt2" title="browser id">' + browserId + '</code> <code class="debug-detail" title="result">' + browserFeatureResult + '</code>';
+                    cell.content += '<br><br><br><br><br><code class="debug-detail-alt" title="feature id">' + feature.id + '</code> <code class="debug-detail-alt2" title="browser id">' + browserId + '</code> <code class="debug-detail" title="result">' + browserFeatureHistory + '</code>';
                 }
 
                 output += '<td class="' + cell.className + '"> ' + cell.content + '</td>';
@@ -152,6 +263,13 @@ function loadTable(payload, locale, isDebug) {
             if(obj.id === id) match = obj;
         });
         return match;
+    }
+
+    function substitute(str, object) {
+        return str.replace((/\\?\{([^{}]+)\}/g), function(match, name) {
+            if (match.charAt(0) == '\\') return match.slice(1);
+            return (object[name] != null) ? object[name] : '';
+        });
     }
 
     function log(data) {
